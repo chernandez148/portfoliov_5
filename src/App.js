47ba773b36e1b5b030eb3ccd8ca7650ce94be9b7
chernandez148 <66753093+chernandez148@users.noreply.github.com>
2023-08-29 08:55:17 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
+import About from './components/About/About';
 import Hero from './components/Hero/Hero';
-import Navbar from './components/Navbar/Navbar';
 import Projects from './components/Projects/Projects';
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Hero />
       <Projects />
+      <About />
+      <Footer />
     </div>
   );
 }
